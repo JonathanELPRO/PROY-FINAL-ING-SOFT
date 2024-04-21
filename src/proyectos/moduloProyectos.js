33ea5e2f551fd62f4a7887075f20506b99fd3ed7
Jonathan <jonathan.moruno@ucb.edu.bo>
2024-04-21 18:01:13 -0400

    Caso de prueba donde se ve el mensaje: Llene todos los campos cuando no se envio la descripcion en el formulario

--- src/proyectos/moduloProyectos.js
+++ src/proyectos/moduloProyectos.js
@@ -2,6 +2,10 @@ function datosFormulariosSonValidos(titulo, descripcion) {
     if(titulo == ""){
       return "Llene todos los campos"
     }
+    else if(descripcion == ""){
+      return "Llene todos los campos"
+    }
+
 }
 
 export default datosFormulariosSonValidos;
--- src/proyectos/moduloProyectos.spec.js
+++ src/proyectos/moduloProyectos.spec.js
@@ -4,4 +4,7 @@ describe("formularioProyectos", () => {
   it("Deberia ver  el mensaje Llene todos los campos si no se lleno el campo de titulo", () => {
     expect(datosFormulariosSonValidos("","descripcion1")).toEqual("Llene todos los campos");
   });
+  it("Deberia ver  el mensaje Llene todos los campos si no se lleno el campo de descripcion", () => {
+    expect(datosFormulariosSonValidos("titulo1","")).toEqual("Llene todos los campos");
+  });
 });
